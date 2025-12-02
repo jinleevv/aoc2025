1501a1cbc93b31710069b504d1baf52cda5dd888
Jin Won Lee <jinwonlee0916@gmail.com>
2025-12-02 16:58:43 -0500

    day 2 hopefully I try Go this week. School too busy

---
+++ day2/part2.py
@@ -0,0 +1,29 @@
+def repeat_sequence(num):
+    num = str(num)
+    n = len(num)
+    for i in range(1, n // 2 + 1):
+        if n % i == 0:
+            pattern = num[:i]
+            if pattern * (n // i) == num:
+                return True
+    
+    return False
+
+def main():
+    f = open("day2/data.txt")
+    data = f.readlines()
+    ranges = data[0].split(",")
+
+    res = 0    
+    for ran in ranges:
+        start, end = int(ran.split("-")[0]), int(ran.split("-")[1])
+        for i in range(start, end):
+            # repeat sequence twice check
+            if repeat_sequence(i):
+                res += i
+    
+    print(res)
+
+
+if __name__ == "__main__":
+    main()
