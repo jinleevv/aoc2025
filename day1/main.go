1360153e2d0b0bb56ff2d83b139204fa062091d9
Jin Won Lee <jinwonlee0916@gmail.com>
2025-12-05 15:50:27 -0500

    try go for day1 part 1

---
+++ day1/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+func main() {
+	fmt.Println("Solving Day 1")
+	num := 50
+	res := 0
+
+	// open a file in GoLang
+	file, err := os.Open("data.txt")
+	if err != nil {
+		log.Fatalf("Input was not able to load properly: %s", err)
+	}
+	// make sure the file is closed
+	defer file.Close()
+
+	// initialize the sanner
+	scanner := bufio.NewScanner(file)
+
+	// start 
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		direction := line[0]
+		rotationStr := line[1:]
+		
+		// convert byte to int
+		rotation, err := strconv.Atoi(rotationStr)
+		if err != nil {
+			log.Printf("Failed to parse number: %s", line)
+		}
+		
+		// make sure you match type with `byte`
+		if direction == 'R' {
+			num += rotation
+		} else {
+			num -= rotation
+		}
+
+		num = (num % 100 + 100) % 100
+
+		if num == 0 {
+			res ++
+		}
+	}
+	// check for any errors while scanning
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+	// print the result
+	fmt.Println(res)
+}
+
+// answer: 1040
