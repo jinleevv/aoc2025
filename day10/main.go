adde7f0ab0333125679a407f927a66fc71fd5a79
Jin Won Lee <jinwonlee0916@gmail.com>
2025-12-26 03:08:33 -0500

    day 10 part 2

--- day10/main.go
+++ day10/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"log"
+	"math"
 	"os"
 	"regexp"
 	"strconv"
@@ -81,18 +82,159 @@ func part1(file *os.File) int {
 		totalPresses += presses
 	}
 
-
 	return totalPresses
 }
 
+var parensRe = regexp.MustCompile(`\(([\d,]+)\)`)
+var curlyRe = regexp.MustCompile(`\{(.*?)\}`)
+
+func solvePart2(file *os.File) int {
+	totalMinPresses := 0
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.TrimSpace(line) == "" { continue }
+
+		// parse given input
+		curlyMatch := curlyRe.FindStringSubmatch(line)
+		if curlyMatch == nil { continue }
+		
+		targetStrs := strings.Split(curlyMatch[1], ",")
+		var targets []int
+		for _, s := range targetStrs {
+			val, _ := strconv.Atoi(strings.TrimSpace(s))
+			targets = append(targets, val)
+		}
+
+		numEqs := len(targets)
+		
+		eqToButtons := make([][]int, numEqs)
+		btnToEqs := make([][]int, 0)
+		
+		parenMatches := parensRe.FindAllStringSubmatch(line, -1)
+		numVars := len(parenMatches)
+
+		for btnIdx, match := range parenMatches {
+			parts := strings.Split(match[1], ",")
+			var affects []int
+			for _, p := range parts {
+				eqIdx, _ := strconv.Atoi(strings.TrimSpace(p))
+				if eqIdx < numEqs {
+					eqToButtons[eqIdx] = append(eqToButtons[eqIdx], btnIdx)
+					affects = append(affects, eqIdx)
+				}
+			}
+			btnToEqs = append(btnToEqs, affects)
+		}
+
+		minTotal := math.MaxInt32
+		
+		// store answers
+		assignment := make([]int, numVars)
+		for i := range assignment { assignment[i] = -1 }
+		
+		currentEqValues := make([]int, numEqs)
+		
+		// backtrack
+		var solve func(currentCost int)
+		solve = func(currentCost int) {
+			if currentCost >= minTotal { return }
+
+			// find the best way to start solving the problem
+			bestEq := -1
+			minUnknowns := math.MaxInt32
+
+			allSatisfied := true
+			
+			for i := 0; i < numEqs; i++ {
+				rem := targets[i] - currentEqValues[i]
+				
+				// check how many buttons are available
+				unknownCount := 0
+				
+				for _, btnIdx := range eqToButtons[i] {
+					if assignment[btnIdx] == -1 {
+						unknownCount++
+					}
+				}
+
+				if unknownCount == 0 {
+					if rem != 0 {
+						return
+					}
+				} else {
+					allSatisfied = false
+					if unknownCount < minUnknowns {
+						minUnknowns = unknownCount
+						bestEq = i
+					}
+				}
+			}
+
+			if allSatisfied {
+				if currentCost < minTotal {
+					minTotal = currentCost
+				}
+				return
+			}
+
+			var varsToAssign []int
+			for _, btnIdx := range eqToButtons[bestEq] {
+				if assignment[btnIdx] == -1 {
+					varsToAssign = append(varsToAssign, btnIdx)
+				}
+			}
+			
+			pivotVar := varsToAssign[0]
+
+			rem := targets[bestEq] - currentEqValues[bestEq]
+			
+			if rem < 0 { return }
+
+			start, end := 0, rem
+			if minUnknowns == 1 {
+				start, end = rem, rem
+			}
+
+			for k := start; k <= end; k++ {
+				assignment[pivotVar] = k
+				
+				validMove := true
+				for _, eqIdx := range btnToEqs[pivotVar] {
+					currentEqValues[eqIdx] += k
+					if currentEqValues[eqIdx] > targets[eqIdx] {
+						validMove = false
+					}
+				}
+
+				if validMove {
+					solve(currentCost + k)
+				}
+
+				for _, eqIdx := range btnToEqs[pivotVar] {
+					currentEqValues[eqIdx] -= k
+				}
+				assignment[pivotVar] = -1
+			}
+		}
+
+		solve(0)
+
+		if minTotal != math.MaxInt32 {
+			totalMinPresses += minTotal
+		}
+	}
+
+	return totalMinPresses
+}
 
 func main() {
 	file, err := os.Open("data.txt")
-
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	answer1 := part1(file)
-	fmt.Println("Answer 1:", answer1)
+	defer file.Close()
+	
+	fmt.Println("Answer Part 2:", solvePart2(file))
 }
